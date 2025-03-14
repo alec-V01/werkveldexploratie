a76fca7b543ad975346a1536d1b692fc01f5d2fa
Lars De Richter <lars.derichter@gmail.com>
2025-03-14 16:03:23 +0100

    Created Dog class

---
+++ Dog.java
@@ -0,0 +1,18 @@
+public class Dog {
+    private String name;
+    private int age;
+
+    Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+     public void bark() {
+        System.out.println("Woof!");
+     }
+}
