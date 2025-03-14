cb0c28cf0e6567891bfd640f364e3e085075119b
Lars De Richter <lars.derichter@gmail.com>
2025-03-14 16:09:28 +0100

    Added setters for Dog

--- Dog.java
+++ Dog.java
@@ -6,6 +6,15 @@ public class Dog {
         this.name = name;
         this.age = age;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
